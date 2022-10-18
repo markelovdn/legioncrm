0936a43908f2e26ba19df0402e8f539dea98a2c1
markelovdn <markelovdn@gmail.com>
2022-10-18 09:34:04 +0300

    'fix2'

--- app/Models/Address.php
+++ app/Models/Address.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Address extends Model
+class Address extends Model implements Authenticatable
 {
     use HasFactory;
 
@@ -27,5 +28,35 @@ class Address extends Model
     public function users()
     {
         return $this->belongsToMany(User::class);
+    }
+
+    public function getAuthIdentifierName()
+    {
+        // TODO: Implement getAuthIdentifierName() method.
+    }
+
+    public function getAuthIdentifier()
+    {
+        // TODO: Implement getAuthIdentifier() method.
+    }
+
+    public function getAuthPassword()
+    {
+        // TODO: Implement getAuthPassword() method.
+    }
+
+    public function getRememberToken()
+    {
+        // TODO: Implement getRememberToken() method.
+    }
+
+    public function setRememberToken($value)
+    {
+        // TODO: Implement setRememberToken() method.
+    }
+
+    public function getRememberTokenName()
+    {
+        // TODO: Implement getRememberTokenName() method.
     }
 }
