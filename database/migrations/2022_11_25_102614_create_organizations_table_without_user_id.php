c79d7cbd575fc4cf04699ba605965a97704471e6
markelovdn <markelovdn@gmail.com>
2022-11-27 17:19:30 +0300

    'fix-organization-migrate-del_userid'

--- database/migrations/2022_11_25_102614_create_organizations_table_without_user_id.php
+++ database/migrations/2022_11_25_102614_create_organizations_table_without_user_id.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 class CreateOrganizationsTableWithoutUserId extends Migration
@@ -14,6 +15,21 @@ class CreateOrganizationsTableWithoutUserId extends Migration
     public function up()
     {
         Schema::table('organizations', function (Blueprint $table) {
+
+            $orgs = DB::table('organizations')
+                ->where('user_id', '!=', null)
+                ->get(['*']);
+
+            foreach ($orgs as $org) {
+                DB::table('organization_user')->insert([
+                    ['organization_id' => $org->id, 'user_id' => $org->user_id,]
+                ]);
+
+                DB::table('organizationS')
+                    ->where('id', '!=', null)
+                    ->update(['user_id' => null]);
+            }
+
             $table->dropForeign('organizations_user_id_foreign');
             $table->dropColumn('user_id');
         });
