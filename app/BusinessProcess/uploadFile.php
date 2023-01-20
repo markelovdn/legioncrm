55633e5d70d61ef68b4a7dd2bc87a84b3afe5b8f
Dmitriy <markelovdn@gmail.com>
2023-01-21 00:00:11 +0300

    fixuploadurl

--- app/BusinessProcess/uploadFile.php
+++ app/BusinessProcess/uploadFile.php
@@ -8,7 +8,7 @@ class uploadFile
 {
     public function uploadFile($id, $user_secondname, $user_firstname, $prefix, $file)
     {
-        $main_url = env('AWS_ENDPOINT').'/'.env('AWS_BUCKET').'/';
+        $main_url = config('filesystems.disks.s3.endpoint').'/'.config('filesystems.disks.s3.bucket').'/';
 
         $filecontent = $file->openFile()->fread($file->getSize());
         $filename = $id.'_'.$user_secondname.'_'.$user_firstname.
