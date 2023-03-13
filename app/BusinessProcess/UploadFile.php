c41bbd9c31deb24009089ca05fdf1728649514ee
Dmitriy <markelovdn@gmail.com>
2023-03-13 11:03:55 +0300

    fix upload file extension

--- app/BusinessProcess/UploadFile.php
+++ app/BusinessProcess/UploadFile.php
@@ -9,48 +9,47 @@ class UploadFile
 {
     public function uploadFile($id, $user_secondname, $user_firstname, $prefix, $file)
     {
-        if ($file->extension() != 'pdf' || $file->extension() != 'PDF'
-            || $file->extension() != 'jpg' || $file->extension() != 'jpeg'
-            || $file->extension() != 'png')
-        {
+        if ($file->extension() == 'pdf' || $file->extension() == 'PDF'
+            || $file->extension() == 'jpg' || $file->extension() == 'jpeg'
+            || $file->extension() == 'png') {
+
+            $main_url = config('filesystems.disks.s3.endpoint').'/'.config('filesystems.disks.s3.bucket').'/';
+
+            $filename = $id.'_'.$user_secondname.'_'.$user_firstname.
+                '/'.$prefix.'_'.$id.'_'.$user_secondname.'_'.$user_firstname.'.jpg';
+
+            if ($file->extension() == 'pdf' || $file->extension() == 'PDF') {
+                $filename = $id.'_'.$user_secondname.'_'.$user_firstname.
+                    '/'.$prefix.'_'.$id.'_'.$user_secondname.'_'.$user_firstname.'.pdf';
+                if ($prefix) {
+                    Storage::disk('s3')->put($filename, $file->openFile()->fread($file->getSize()));
+                }
+                return $main_url.$filename;
+            }
+
+            switch ($prefix) {
+                case ('photo'):
+                    $resize = 354;
+                    break;
+                default:
+                    $resize = 520;
+            };
+
+            $img = Image::make($file)->resize($resize, null, function ($constraint) {
+                $constraint->aspectRatio();
+            });
+
+            $filecontent = $img->stream();
+
+            if ($prefix) {
+                Storage::disk('s3')->put($filename, $filecontent->__toString());
+            }
+
+            return $main_url.$filename;
+        } else {
             return false;
         }
 
-        $main_url = config('filesystems.disks.s3.endpoint').'/'.config('filesystems.disks.s3.bucket').'/';
-
-        $filename = $id.'_'.$user_secondname.'_'.$user_firstname.
-            '/'.$prefix.'_'.$id.'_'.$user_secondname.'_'.$user_firstname.'.jpg';
-
-        if ($file->extension() == 'pdf' || $file->extension() == 'PDF') {
-            $filename = $id.'_'.$user_secondname.'_'.$user_firstname.
-                '/'.$prefix.'_'.$id.'_'.$user_secondname.'_'.$user_firstname.'.pdf';
-            if ($prefix) {
-                Storage::disk('s3')->put($filename, $file->openFile()->fread($file->getSize()));
-            }
-            return $main_url.$filename;
-        }
-
-        switch ($prefix) {
-            case ('photo'):
-                $resize = 354;
-                break;
-            default:
-                $resize = 520;
-        };
-
-        $img = Image::make($file)->resize($resize, null, function ($constraint) {
-        $constraint->aspectRatio();
-                });
-
-
-
-        $filecontent = $img->stream();
-
-        if ($prefix) {
-            Storage::disk('s3')->put($filename, $filecontent->__toString());
-        }
-
-        return $main_url.$filename;
     }
 }
 
