acd150f96fc4ac79c3574de527b402ba7dbeb792
Dmitriy <markelovdn@gmail.com>
2023-05-16 14:07:48 +0300

    fix print doc athlete compact

--- app/BusinessProcess/PrintAthleteDocument.php
+++ app/BusinessProcess/PrintAthleteDocument.php
@@ -18,28 +18,28 @@ class PrintAthleteDocument
     public function printCscaCard(Request $request)
     {
         $athlete = Athlete::with('user', 'birthcertificate', 'parenteds')->find($request->id);
-        $pdf = Pdf::loadView('athletes.print.cska-card', compact('athlete', $athlete))->setPaper('A4', 'landscape');
+        $pdf = Pdf::loadView('athletes.print.cska-card', ['athlete' => $athlete])->setPaper('A4', 'landscape');
         return $pdf->stream('csa-card.pdf');
     }
 
     public function printAgreementParented(Request $request)
     {
         $athlete = Athlete::with('user', 'birthcertificate', 'parenteds')->find($request->id);
-        $pdf = Pdf::loadView('athletes.print.agreement-parented-attestation-federation', compact('athlete', $athlete))->setPaper('A4', 'portrait');
+        $pdf = Pdf::loadView('athletes.print.agreement-parented-attestation-federation', ['athlete' => $athlete])->setPaper('A4', 'portrait');
         return $pdf->stream('agreement-parented_'.$athlete->user->secondname.' '.$athlete->user->firstname.' '.$athlete->user->patronymic.'.pdf');
     }
 
     public function printCskaStatementSOG(Request $request)
     {
         $athlete = Athlete::with('user', 'birthcertificate', 'parenteds', 'studyplace')->find($request->id);
-        $pdf = Pdf::loadView('athletes.print.cska-statement-SOG', compact('athlete', $athlete))->setPaper('A4', 'portrait');
+        $pdf = Pdf::loadView('athletes.print.cska-statement-SOG', ['athlete' => $athlete])->setPaper('A4', 'portrait');
         return $pdf->stream('заявление СОГ '.$athlete->user->secondname.' '.$athlete->user->firstname.' '.$athlete->user->patronymic.'.pdf');
     }
 
     public function printCskaStatement(Request $request)
     {
         $athlete = Athlete::with('user', 'birthcertificate', 'parenteds')->find($request->id);
-        $pdf = Pdf::loadView('athletes.print.cska-statement', compact('athlete', $athlete))->setPaper('A4', 'portrait');
+        $pdf = Pdf::loadView('athletes.print.cska-statement', ['athlete' => $athlete])->setPaper('A4', 'portrait');
         return $pdf->stream('заявление ЦСКА '.$athlete->user->secondname.' '.$athlete->user->firstname.' '.$athlete->user->patronymic.'.pdf');
     }
 
