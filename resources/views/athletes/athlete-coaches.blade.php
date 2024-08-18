717d1ed21f22ba61b327be32f73e726062081fc9
Дмитрий <markelovdn@gmail.com>
2024-08-18 12:24:02 +0300

    тест для деплоя

--- resources/views/athletes/athlete-coaches.blade.php
+++ resources/views/athletes/athlete-coaches.blade.php
@@ -8,40 +8,40 @@
         </div>
     </div>
     <div class="card-body" style="display: none;">
-            @csrf
-            <div class="card-body">
-                <dl class="row">
-                    @foreach($athlete->getCoachesAthlete($athlete->id)->coaches as $coach)
-                        @if($coach->pivot->coach_type == \App\Models\Coach::FIRST_COACH)
-                        <dt class="col-sm-4">Первый тренер</dt>
-                        <dd class="col-sm-8">{{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.</dd>
-                        @endif
-                        @if($coach->pivot->coach_type == \App\Models\Coach::SECOND_COACH)
-                            <dt class="col-sm-4">Второй тренер</dt>
-                            <dd class="col-sm-8">{{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.</dd>
-                        @endif
-                        @if($coach->pivot->coach_type == \App\Models\Coach::THIRD_COACH)
-                            <dt class="col-sm-4">Третий тренер</dt>
-                            <dd class="col-sm-8">{{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.</dd>
-                        @endif
-                        @if($coach->pivot->coach_type == \App\Models\Coach::REAL_COACH)
-                            <dt class="col-sm-4">Действующий тренер</dt>
-                            <dd class="col-sm-8">{{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.</dd>
-                        @endif
-                    @endforeach
+        @csrf
+        <div class="card-body">
+            <dl class="row">
+                @foreach($athlete->getCoachesAthlete($athlete->id)->coaches as $coach)
+                @if($coach->pivot->coach_type == \App\Models\Coach::FIRST_COACH)
+                <dt class="col-sm-4">Первый тренер</dt>
+                <dd class="col-sm-8">{{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.</dd>
+                @endif
+                @if($coach->pivot->coach_type == \App\Models\Coach::SECOND_COACH)
+                <dt class="col-sm-4">Второй тренер</dt>
+                <dd class="col-sm-8">{{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.</dd>
+                @endif
+                @if($coach->pivot->coach_type == \App\Models\Coach::THIRD_COACH)
+                <dt class="col-sm-4">Третий тренер</dt>
+                <dd class="col-sm-8">{{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.</dd>
+                @endif
+                @if($coach->pivot->coach_type == \App\Models\Coach::REAL_COACH)
+                <dt class="col-sm-4">Действующий тренер</dt>
+                <dd class="col-sm-8">{{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.</dd>
+                @endif
+                @endforeach
 
-                </dl>
-            </div>
-            @php
-                $user = new \App\Models\User();
-            @endphp
-       @switch($user->getRoleCode())
-           @case(\App\Models\Role::ROLE_SYSTEM_ADMIN)
-           @case(\App\Models\Role::ROLE_ORGANIZATION_CHAIRMAN)
-           @case(\App\Models\Role::ROLE_ORGANIZATION_ADMIN)
-               <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default{{$athlete->id}}">
-                   <i class="far fa-edit"></i>
-               </button>
+            </dl>
+        </div>
+        @php
+        $user = new \App\Models\User();
+        @endphp
+        @switch($user->getRoleCode())
+        @case(\App\Models\Role::ROLE_SYSTEM_ADMIN)
+        @case(\App\Models\Role::ROLE_ORGANIZATION_CHAIRMAN)
+        @case(\App\Models\Role::ROLE_ORGANIZATION_ADMIN)
+        <button type="button" class="btn btn-default" data-toggle="modal" data-target="#modal-default{{$athlete->id}}">
+            <i class="far fa-edit"></i>
+        </button>
 
         @endswitch
 
@@ -58,56 +58,56 @@
                             <div class="row mb-3">
                                 <label for="first_coach" class="col-md-4 col-form-label text-md-end">Первый тренер<span class="text-danger">*</span></label>
                                 <div class="col-md-6">
-                                    <select type="text" class="form-control @error('first_coach') is-invalid @enderror"  name="first_coach" id="first_coach">
+                                    <select type="text" class="form-control @error('first_coach') is-invalid @enderror" name="first_coach" id="first_coach">
                                         <option></option>
                                         @foreach($coaches as $coach)
                                         <option value="{{$coach->id}}">
                                             {{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.
                                         </option>
                                         @endforeach
-                                             TODO: делать запросы из вьюх это плоо
+                                        TODO: делать запросы из вьюх это плоо
                                     </select>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <label for="second_coach" class="col-md-4 col-form-label text-md-end">Второй тренер<span class="text-danger">*</span></label>
                                 <div class="col-md-6">
-                                    <select type="text" class="form-control @error('second_coach') is-invalid @enderror"  name="second_coach" id="second_coach">
+                                    <select type="text" class="form-control @error('second_coach') is-invalid @enderror" name="second_coach" id="second_coach">
                                         <option></option>
                                         @foreach($coaches as $coach)
-                                            <option value="{{$coach->id}}">
-                                                {{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.
-                                            </option>
+                                        <option value="{{$coach->id}}">
+                                            {{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.
+                                        </option>
                                         @endforeach
-                                         TODO: делать запросы из втюх это плоо
+                                        TODO: делать запросы из втюх это плоо
                                     </select>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <label for="third_coach" class="col-md-4 col-form-label text-md-end">Третий тренер<span class="text-danger">*</span></label>
                                 <div class="col-md-6">
-                                    <select type="text" class="form-control @error('third_coach') is-invalid @enderror"  name="third_coach" id="third_coach">
+                                    <select type="text" class="form-control @error('third_coach') is-invalid @enderror" name="third_coach" id="third_coach">
                                         <option></option>
                                         @foreach($coaches as $coach)
-                                            <option value="{{$coach->id}}">
-                                                {{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.
-                                            </option>
+                                        <option value="{{$coach->id}}">
+                                            {{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.
+                                        </option>
                                         @endforeach
-                                         TODO: делать запросы из втюх это плоо
+                                        TODO: делать запросы из втюх это плоо
                                     </select>
                                 </div>
                             </div>
                             <div class="row mb-3">
                                 <label for="real_coach" class="col-md-4 col-form-label text-md-end">Действующий тренер<span class="text-danger">*</span></label>
                                 <div class="col-md-6">
-                                    <select type="text" class="form-control @error('real_coach') is-invalid @enderror"  name="real_coach" id="real_coach">
+                                    <select type="text" class="form-control @error('real_coach') is-invalid @enderror" name="real_coach" id="real_coach">
                                         <option></option>
                                         @foreach($coaches as $coach)
-                                            <option value="{{$coach->id}}">
-                                                {{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.
-                                            </option>
+                                        <option value="{{$coach->id}}">
+                                            {{$coach->user->secondname}} {{mb_substr($coach->user->firstname, 0,1)}}.{{mb_substr($coach->user->patronymic, 0,1)}}.
+                                        </option>
                                         @endforeach
-                                         TODO: делать запросы из втюх это плоо
+                                        TODO: делать запросы из втюх это плоо
                                     </select>
                                 </div>
                             </div>
--- resources/views/competitors/print/competitor-certificate.blade.php
+++ resources/views/competitors/print/competitor-certificate.blade.php
@@ -1,10 +1,11 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{$competitor->athlete->user->secondname.' '.$competitor->athlete->user->firstname}}</title>
 </head>
@@ -15,65 +16,67 @@
         color: #000;
     }
 </style>
+
 <body>
 
-<div class="container">
-    <div class="row">
-        <div class="col-12">
-            <div class="p-3" style="font-size: xx-large; margin-top: 250; text-align: center;"><strong>НАГРАЖДАЕТСЯ</strong></div>
-            <div class="p-3" style="font-size: x-large; margin-top: 20; text-align: center"><strong>{{$competitor->athlete->user->secondname.' '.$competitor->athlete->user->firstname}}</strong></div>
-        </div>
-        <div class="col-6">
-            <div class="p-3" style="font-size: x-large; text-align: center">
-                <strong>
-                    @if($competitor->place <= 3)
-                        @if($competitor->athlete->gender == \App\Models\Athlete::GENDER_MALE) занявший
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="p-3" style="font-size: xx-large; margin-top: 250; text-align: center;"><strong>НАГРАЖДАЕТСЯ</strong></div>
+                <div class="p-3" style="font-size: x-large; margin-top: 20; text-align: center"><strong>{{$competitor->athlete->user->secondname.' '.$competitor->athlete->user->firstname}}</strong></div>
+            </div>
+            <div class="col-6">
+                <div class="p-3" style="font-size: x-large; text-align: center">
+                    <strong>
+                        @if($competitor->place <= 3)
+                            @if($competitor->athlete->gender == \App\Models\Athlete::GENDER_MALE) занявший
                             @else занявшая @endif {{$competitor->place}} место
-                    @else за активное участие @endif
+                            @else за активное участие @endif
 
-                </strong>
+                    </strong>
+                </div>
             </div>
-        </div>
-        <div class="col-6">
-            <div class="p-3" style="font-size: x-large; text-align: center">в {{Str::of($competition_title)->title()}} {{Str::of($competition->title)->after(' ')}}</div>
-        </div>
-        <div class="col-6">
-            <div class="p-3" style="font-size: x-large; text-align: center">среди
-                @if(Str::of($competitor->agecategory->title)->before(' ') == 'Дети'
+            <div class="col-6">
+                <div class="p-3" style="font-size: x-large; text-align: center">в {{Str::of($competition_title)->title()}} {{Str::of($competition->title)->after(' ')}}</div>
+            </div>
+            <div class="col-6">
+                <div class="p-3" style="font-size: x-large; text-align: center">среди
+                    @if(Str::of($competitor->agecategory->title)->before(' ') == 'Дети'
                     && $competitor->athlete->gender == \App\Models\Athlete::GENDER_MALE) мальчиков
-                @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Дети'
+                    @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Дети'
                     && $competitor->athlete->gender == \App\Models\Athlete::GENDER_FEMALE) девочек
-                @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Юноши'
-                && $competitor->athlete->gender == \App\Models\Athlete::GENDER_MALE) юношей
-                @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Юноши'
-                && $competitor->athlete->gender == \App\Models\Athlete::GENDER_FEMALE) девушек
-                @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Юниоры'
-                && $competitor->athlete->gender == \App\Models\Athlete::GENDER_MALE) юниоров
-                @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Юниоры'
-                && $competitor->athlete->gender == \App\Models\Athlete::GENDER_FEMALE) юниорок
-                @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Взрослые'
-                && $competitor->athlete->gender == \App\Models\Athlete::GENDER_MALE) мужчин
-                @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Взрослые'
-                && $competitor->athlete->gender == \App\Models\Athlete::GENDER_FEMALE) женщин
-                @endif
-                {{Str::of($competitor->agecategory->title)->after(' ')}}
+                    @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Юноши'
+                    && $competitor->athlete->gender == \App\Models\Athlete::GENDER_MALE) юношей
+                    @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Юноши'
+                    && $competitor->athlete->gender == \App\Models\Athlete::GENDER_FEMALE) девушек
+                    @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Юниоры'
+                    && $competitor->athlete->gender == \App\Models\Athlete::GENDER_MALE) юниоров
+                    @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Юниоры'
+                    && $competitor->athlete->gender == \App\Models\Athlete::GENDER_FEMALE) юниорок
+                    @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Взрослые'
+                    && $competitor->athlete->gender == \App\Models\Athlete::GENDER_MALE) мужчин
+                    @elseif(Str::of($competitor->agecategory->title)->before(' ') == 'Взрослые'
+                    && $competitor->athlete->gender == \App\Models\Athlete::GENDER_FEMALE) женщин
+                    @endif
+                    {{Str::of($competitor->agecategory->title)->after(' ')}}
+                </div>
             </div>
-        </div>
-        <div class="col-6">
-           <div class="p-3" style="font-size: x-large; text-align: center">в весовой категории {{$competitor->weightcategory->title}}</div>
-        </div>
-        <div class="col-3">
-            <div class="p-6" style="font-size: medium; margin-top: 100; margin-left: 70">Председатель правления</div>
-            <div class="p-6" style="font-size: medium; margin-top: 0; margin-left: 70">СК "Легион"</div>
-{{--            <div class="p-6" style="font-size: medium; margin-top: 0; margin-left: 70">администрации Волгограда</div>--}}
-        </div>
-        <div class="col-2">
-            <div class="p-3" style="font-size: medium; margin-top: -19; margin-left: 370">Д. Н. Маркелов</div>
-        </div>
-        <div class="col-2">
-            <div style="font-size: medium; margin-top: 30; padding-bottom: -100px; text-align: center">Волгоград, {{ \Carbon\Carbon::parse($competition->date_start)->format('d.m.Y')}}</div>
+            <div class="col-6">
+                <div class="p-3" style="font-size: x-large; text-align: center">в весовой категории {{$competitor->weightcategory->title}}</div>
+            </div>
+            <div class="col-3">
+                <div class="p-6" style="font-size: medium; margin-top: 100; margin-left: 70">Председатель правления</div>
+                <div class="p-6" style="font-size: medium; margin-top: 0; margin-left: 70">СК "Легион"</div>
+                {{-- <div class="p-6" style="font-size: medium; margin-top: 0; margin-left: 70">администрации Волгограда</div>--}}
+            </div>
+            <div class="col-2">
+                <div class="p-3" style="font-size: medium; margin-top: -19; margin-left: 370">Д. Н. Маркелов</div>
+            </div>
+            <div class="col-2">
+                <div style="font-size: medium; margin-top: 30; padding-bottom: -100px; text-align: center">Волгоград, {{ \Carbon\Carbon::parse($competition->date_start)->format('d.m.Y')}}</div>
+            </div>
         </div>
     </div>
-</div>
 </body>
+
 </html>
