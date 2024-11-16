21060a48ffca5501d5cadf53f3b91bc1c34f2685
Дмитрий <markelovdn@gmail.com>
2024-11-16 15:19:52 +0300

    изменения по списку участников соревнований

--- resources/js/components/competitors/CompetitorsIndex.vue
+++ resources/js/components/competitors/CompetitorsIndex.vue
@@ -5,35 +5,46 @@
                 <div class="card-header">
                     <h1 class="card-title">{{ competition.title }}</h1>
                     <div class="card-tools">
-                        <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                        <button
+                            type="button"
+                            class="btn btn-tool"
+                            data-card-widget="collapse"
+                            title="Collapse"
+                        >
                             <i class="fas fa-plus"></i>
                         </button>
                     </div>
                 </div>
-                <div class="card-body" style="display: none;" v-if="!user.parented">
-                    <coach-select :competition_id="competition.id"
-                                  :is_owner="is_owner"
-                                  :user="user"></coach-select>
-                    <age-categories-select :competition_id="competition.id"></age-categories-select>
+                <div class="card-body">
+                    <coach-select
+                        :competition_id="competition.id"
+                        :is_owner="is_owner"
+                        :user="user"
+                    ></coach-select>
+                    <age-categories-select
+                        :competition_id="competition.id"
+                    ></age-categories-select>
                     <weight-categories-select></weight-categories-select>
-                    <teh-kval-group-select :competition_id="competition.id"></teh-kval-group-select>
+                    <teh-kval-group-select
+                        :competition_id="competition.id"
+                    ></teh-kval-group-select>
                 </div>
             </div>
         </div>
 
-        <competitors-list :competition_id="competition.id"
-                          :competition_open_registration="competition.open_registration"
-                          :key="competition.id"
-                          :is_owner="is_owner"
-                          :user="user"></competitors-list>
-
+        <competitors-list
+            :competition_id="competition.id"
+            :competition_open_registration="competition.open_registration"
+            :key="competition.id"
+            :is_owner="is_owner"
+            :user="user"
+        ></competitors-list>
     </div>
-
 </template>
 
 <script>
-import CompetitorsList from './CompetitorsList'
-import CoachSelect from './filters/CoachSelect'
+import CompetitorsList from "./CompetitorsList";
+import CoachSelect from "./filters/CoachSelect";
 import AgeCategoriesSelect from "./filters/AgeCategoriesSelect";
 import WeightCategoriesSelect from "./filters/WeightCategoriesSelect";
 import TehKvalGroupSelect from "./filters/TehKvalGroupSelect";
@@ -41,37 +52,25 @@ import TehKvalGroupSelect from "./filters/TehKvalGroupSelect";
 export default {
     name: "competitors-index",
     data() {
-        return {
-
-        }
+        return {};
     },
     props: {
-      competition: Object,
-      coach_constant: Array,
-      is_owner: Number,
-      user: Object
+        competition: Object,
+        coach_constant: Array,
+        is_owner: Number,
+        user: Object,
     },
     components: {
         TehKvalGroupSelect,
         CompetitorsList,
         CoachSelect,
         AgeCategoriesSelect,
-        WeightCategoriesSelect
+        WeightCategoriesSelect,
     },
-    created() {
-
-    },
-    methods: {
-
-    },
-    mounted() {
-
-    }
-
-}
-
+    created() {},
+    methods: {},
+    mounted() {},
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- resources/views/competitors/competitors.blade.php
+++ resources/views/competitors/competitors.blade.php
@@ -1,106 +1,105 @@
 @extends('layouts.main')
 @section('content')
-	<!-- Content Header (Page header) -->
-	<div class="content-header">
-		<div class="card collapsed-card">
-			<div class="card-header">
-				<h1 class="card-title">@foreach($competition as $item){{$item->name}}@endforeach</h1>
-				<div class="card-tools">
-					<button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-						<i class="fas fa-plus"></i>
-					</button>
-				</div>
-			</div>
-			<div class="card-body" style="display: none;">
-				@foreach($competition as $item)Дата начала:{{ \Carbon\Carbon::parse($item->date_start)->format('d.m.Y')}}@endforeach
-			</div>
-		</div>
-
-	</div>
-	<!-- /.content-header -->
-	<section class="content">
-		@if (session('status'))
-			<div class="alert alert-success">
-				{{ session('status') }}
-			</div>
-		@endif
-		@foreach ($competitors as $competitor)
-		<div @if($competitor->gender == 'мужской')class="card card-primary collapsed-card" @else class="card card-danger collapsed-card" @endif>
-			<div class="card-header">
-				<h3 class="card-title">{{$competitor->secondname}} {{$competitor->firstname}} {{$competitor->patronymic}}</h3><br>
-				@if($competitor->gender == 'мужской')
-					<i class="fas fa-male"></i>
-				@else
-					<i class="fas fa-female"></i>
-				@endif - {{$competitor->agecategory->name}}, {{$competitor->weightcategory->name}}, {{$competitor->tehkvalgroup->name}}
-				Тренер: {{$competitor->coach->secondname}} {{substr($competitor->coach->firstname, 0,2)}}. {{substr($competitor->coach->patronymic, 0, 2)}}.
-				<div class="card-tools">
-					<button type="button" class="btn btn-tool" data-card-widget="collapse">
-						<i class="fas fa-plus"></i>
-					</button>
-				</div>
-			</div>
-			<div class="card-body" style="display: none;">
-				<b>Дата рождения: </b> {{ \Carbon\Carbon::parse($competitor->date_of_birth)->format('d.m.Y')}}<br>
-				<b>Вес: </b>{{$competitor->weight}}<br>
-				<b>Техническая квалификация: </b>{{$competitor->tehkval->name}}<br>
-				<b>Спортивная квалификация: </b>{{$competitor->sportkval->fullname}}<br>
-			</div>
-
-		</div>
-			@endforeach
-	</section>
-{{--    {{dd($pair)}}--}}
-    <header class="hero">
-        <div class="hero-wrap">
-            <p class="intro" id="intro">flexbox</p>
-            <h1 id="headline">Tournament</h1>
-            <p class="year"><i class="fa fa-star"></i> 2015 <i class="fa fa-star"></i></p>
-            <p>Ballin' Outta Control</p>
+<!-- Content Header (Page header) -->
+<div class="content-header">
+    <div class="card collapsed-card">
+        <div class="card-header">
+            <h1 class="card-title">@foreach($competition as $item){{$item->name}}@endforeach</h1>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                    <i class="fas fa-plus"></i>
+                </button>
+            </div>
         </div>
-    </header>
+        <div class="card-body" style="display: none;">
+            @foreach($competition as $item)Дата начала:{{ \Carbon\Carbon::parse($item->date_start)->format('d.m.Y')}}@endforeach
+        </div>
+    </div>
 
-    <section id="bracket">
-        <div class="container">
-            @foreach($pair as $item)
-                @if($loop->first)
-            <div class="split split-one">
-                <div class="round round-one current">
-                    <div class="round-details">Round 1<br/><span class="date">March 16</span></div>
-                    @foreach($item as $a)
-                    <ul class="matchup">
-                        @foreach($a as $b)
-                        <li class="team team-top">{{$b['secondname']}}<span class="score">{{$b['lot']}}</span></li>
-                        @endforeach
-                    </ul>
-                    @endforeach
-                </div>	<!-- END ROUND ONE -->
+</div>
+<!-- /.content-header -->
+<section class="content">
+    @if (session('status'))
+    <div class="alert alert-success">
+        {{ session('status') }}
+    </div>
+    @endif
+    @foreach ($competitors as $competitor)
+    <div @if($competitor->gender == 'мужской')class="card card-primary collapsed-card" @else class="card card-danger collapsed-card" @endif>
+        <div class="card-header">
+            <h3 class="card-title">{{$competitor->secondname}} {{$competitor->firstname}} {{$competitor->patronymic}}</h3><br>
+            @if($competitor->gender == 'мужской')
+            <i class="fas fa-male"></i>
+            @else
+            <i class="fas fa-female"></i>
+            @endif - {{$competitor->agecategory->name}}, {{$competitor->weightcategory->name}}, {{$competitor->tehkvalgroup->name}}
+            Тренер: {{$competitor->coach->secondname}} {{substr($competitor->coach->firstname, 0,2)}}. {{substr($competitor->coach->patronymic, 0, 2)}}.
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-plus"></i>
+                </button>
             </div>
-                @endif
-                    @if($loop->last)
-            <div class="split split-two">
-                <div class="round round-one current">
-                    <div class="round-details">Round 1<br/><span class="date">March 16</span></div>
-                    @foreach($item as $a)
-                    <ul class="matchup">
-                            @foreach($a as $b)
-                            <li class="team team-top">{{$b['secondname']}}<span class="score">{{$b['lot']}}</span></li>
-                            @endforeach
-                    </ul>
+        </div>
+        <div class="card-body" style="display: none;">
+            <b>Дата рождения: </b> {{ \Carbon\Carbon::parse($competitor->date_of_birth)->format('d.m.Y')}}<br>
+            <b>Вес: </b>{{$competitor->weight}}<br>
+            <b>Техническая квалификация: </b>{{$competitor->tehkval->name}}<br>
+            <b>Спортивная квалификация: </b>{{$competitor->sportkval->fullname}}<br>
+        </div>
+
+    </div>
+    @endforeach
+</section>
+{{-- {{dd($pair)}}--}}
+<header class="hero">
+    <div class="hero-wrap">
+        <p class="intro" id="intro">flexbox</p>
+        <h1 id="headline">Tournament</h1>
+        <p class="year"><i class="fa fa-star"></i> 2015 <i class="fa fa-star"></i></p>
+        <p>Ballin' Outta Control</p>
+    </div>
+</header>
+
+<section id="bracket">
+    <div class="container">
+        @foreach($pair as $item)
+        @if($loop->first)
+        <div class="split split-one">
+            <div class="round round-one current">
+                <div class="round-details">Round 1<br /><span class="date">March 16</span></div>
+                @foreach($item as $a)
+                <ul class="matchup">
+                    @foreach($a as $b)
+                    <li class="team team-top">{{$b['secondname']}}<span class="score">{{$b['lot']}}</span></li>
                     @endforeach
-                </div>	<!-- END ROUND ONE -->
-            </div>
-                    @endif
+                </ul>
                 @endforeach
+            </div> <!-- END ROUND ONE -->
         </div>
-    </section>
-    <section class="share">
-        <div class="share-wrap">
-            <a class="share-icon" href="https://twitter.com/_joebeason"><i class="fa fa-twitter"></i></a>
-            <a class="share-icon" href="#"><i class="fa fa-facebook"></i></a>
-            <a class="share-icon" href="#"><i class="fa fa-envelope"></i></a>
+        @endif
+        @if($loop->last)
+        <div class="split split-two">
+            <div class="round round-one current">
+                <div class="round-details">Round 1<br /><span class="date">March 16</span></div>
+                @foreach($item as $a)
+                <ul class="matchup">
+                    @foreach($a as $b)
+                    <li class="team team-top">{{$b['secondname']}}<span class="score">{{$b['lot']}}</span></li>
+                    @endforeach
+                </ul>
+                @endforeach
+            </div> <!-- END ROUND ONE -->
         </div>
-    </section>
+        @endif
+        @endforeach
+    </div>
+</section>
+<section class="share">
+    <div class="share-wrap">
+        <a class="share-icon" href="https://twitter.com/_joebeason"><i class="fa fa-twitter"></i></a>
+        <a class="share-icon" href="#"><i class="fa fa-facebook"></i></a>
+        <a class="share-icon" href="#"><i class="fa fa-envelope"></i></a>
+    </div>
+</section>
 
 @endsection
-
